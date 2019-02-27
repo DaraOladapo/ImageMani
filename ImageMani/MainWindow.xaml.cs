11e3c921faa376dfae4a25b8214f7418d3bb9ac8
Dara Oladapo <daraoladapo@outlook.com>
2019-02-28 00:04:18 +0100

    Refactored to class library

--- ImageMani/MainWindow.xaml.cs
+++ ImageMani/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using ImageMani.Library;
+using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using static System.Environment;
 
 namespace ImageMani
 {
@@ -23,7 +23,6 @@ namespace ImageMani
             ofd.FileOk += Ofd_FileOk;
             ofd.Filter = "JPG files (*.jpg)|*.jpg|PNG files (*.jpg)|*.png|JPEG files (*.jpg)|*.jpeg";
             ofd.Multiselect = false;
-            ofd.InitialDirectory = GetFolderPath(SpecialFolder.MyPictures);
             ofd.ShowDialog();
         }
 
@@ -36,43 +35,16 @@ namespace ImageMani
                 bitmap.UriSource = new Uri(ofd.FileName);
                 bitmap.EndInit();
                 LoadedImage.Source = bitmap;
-                ImageToBase64Label.Text = ConvertImageToBase64();
-            }
-
-        }
-
-        private string ConvertImageToBase64()
-        {
-            using (System.Drawing.Image image = System.Drawing.Image.FromFile(ofd.FileName))
-            {
-                using (MemoryStream m = new MemoryStream())
-                {
-                    image.Save(m, image.RawFormat);
-                    byte[] imageBytes = m.ToArray();
-                    return Convert.ToBase64String(imageBytes);
-                }
+                ImageToBase64Label.Text = ImageConverter.ConvertImageToBase64(ofd.FileName);
             }
         }
         private void OnConvertTextToImage(object sender, RoutedEventArgs e)
         {
             BitmapImage bitmap = new BitmapImage();
             bitmap.BeginInit();
-            bitmap.StreamSource = new MemoryStream(ConvertFromBase64ToByteArray(ImageToBase64Label.Text));
+            bitmap.StreamSource = new MemoryStream(ImageConverter.ConvertFromBase64ToByteArray(ImageToBase64Label.Text));
             bitmap.EndInit();
             ConvertedImage.Source = bitmap;
-        }
-        private byte[] ConvertFromBase64ToByteArray(string baseString)
-        {
-            byte[] imageBytes = Convert.FromBase64String(baseString);
-            return imageBytes;
-        }
-        private System.Drawing.Image ConvertImageFromBase64(string baseString)
-        {
-            byte[] imageBytes = Convert.FromBase64String(baseString);
-            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
-            ms.Write(imageBytes, 0, imageBytes.Length);
-            System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
-            return image;
         }
     }
 }
